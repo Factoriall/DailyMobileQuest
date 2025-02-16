563ea4c27cc778990d1ddd475d08b05365aff9d9
fac.toriall <fac.toriall@kakaocorp.com>
2025-02-16 22:26:24 +0900

    Alarm.kt toString 수정

--- app/src/main/java/com/example/dailymobilequest/data/Alarm.kt
+++ app/src/main/java/com/example/dailymobilequest/data/Alarm.kt
@@ -4,23 +4,23 @@ package com.example.dailymobilequest.data
  * 알람 관련 데이터 클래스
  */
 sealed class Alarm {
-    data object No : Alarm()
-    sealed class Yes(open val time: String) : Alarm() {
-        data class Notification(override val time: String) : Yes(time)
-        data class Sound(override val time: String) : Yes(time)
-
+    data object No : Alarm() {
         override fun toString(): String {
-            return when (this) {
-                is Notification -> "노티 $time"
-                is Sound -> "알람 $time"
-            }
+            return "알람 없음"
         }
     }
 
-    override fun toString(): String {
-        return when (this) {
-            is No -> "알람 없음"
-            is Yes -> this.toString()
+    sealed class Yes(open val time: String) : Alarm() {
+        data class Notification(override val time: String) : Yes(time) {
+            override fun toString(): String {
+                return "노티 $time"
+            }
+        }
+
+        data class Sound(override val time: String) : Yes(time) {
+            override fun toString(): String {
+                return "알람 $time"
+            }
         }
     }
 }
